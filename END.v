84d1c16e85ff8890f93f6f8552e18ef9311acab8
JamesArmstrong7 <juansealarcon02@gmail.com>
2026-05-08 15:20:24 -0500

    Stabilize initial ENCR object format implementation

---
+++ END.v
@@ -0,0 +1,10 @@
+Version 0.1.0
+
+Esta version ha llegado a ser
+bastante estable aunque aun cuenta
+con algunos eerores de sintaxis y
+puede reescribirse mejor, por lo que
+finaliza aquí para ir directamente a
+por la 0.2.0, la cual traerá
+mejoras significativas en su estrucutra
+y estabilidad.
